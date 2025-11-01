b99b12a45b2949ae1485d1036fb5138abd87bf74
Fazeel2003-md <mdfazeel93@gmail.com>
2025-11-01 13:22:49 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,11 @@
+#Task-1
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+add=a+b
+print(f'Addition: {add}')
+sub=a-b
+print(f'Subraction: {sub}')
+mul=a*b
+print(f'multiplication: {mul}')
+div=a/b
+print(f'Division: {div}')
